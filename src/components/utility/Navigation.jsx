53381e3f2b0b980ed7e64e891bf64ac6d0cc3e41
josephdanielm <josephmuddjr@gmail.com>
2024-06-23 17:13:47 -0500

    build out navigation component

--- src/components/utility/Navigation.jsx
+++ src/components/utility/Navigation.jsx
@@ -1,3 +1,23 @@
-export default function Navigation() {
-  return <>Navigation</>;
+export default function Navigation({
+  currentStep,
+  stepsLength,
+  setCurrentStep,
+}) {
+  function nextStep() {
+    setCurrentStep((prev) => Math.min(prev + 1, stepsLength - 1));
+  }
+  function prevStep() {
+    setCurrentStep((prev) => Math.max(prev - 1, 0));
+  }
+
+  return (
+    <div className="navigation-wrapper">
+      {currentStep > 0 && <button onClick={prevStep}>&lt;</button>}
+      {currentStep < stepsLength - 1 ? (
+        <button onClick={nextStep}>&gt;</button>
+      ) : (
+        <button onClick={submitForm}>Submit</button>
+      )}
+    </div>
+  );
 }
